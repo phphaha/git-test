d2d3a6a3da19363a11f5183e4588e97b3025c120
php <php@qq.com>
2022-06-02 12:48:51 +0800

    first commit

---
+++ src/main/java/com/atgui/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.atgui.git;
+
+import javafx.beans.value.ObservableBooleanValue;
+
+/**
+ * @Author: 潘弘鹏学Java
+ * @Date: 2022/6/2 - 06 - 02 -12:41
+ * @Description: com.atgui.git
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
